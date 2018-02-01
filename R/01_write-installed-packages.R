6ab977a35b65e23779dfe8d7d9cfa6bc55392cba
Ting Ma <tma@grailbio.com>
2018-02-01 11:51:15 -0800

    finished the first assignment of csv creation

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,10 @@
 ## deja vu from yesterday!
 
+library(tidyverse)
 ## create a data frame of your installed packages
+
+paks <- installed.packages()  %>%
+        as_tibble()
 
 ## keep the variables
 ##   * Package
@@ -9,10 +13,22 @@
 ##   * Priority
 ##   * Built
 
+paks <- paks %>%
+  dplyr::select(1:4, 16)
+
+list.files(path="./R")
+here::here("R", "01_write-installed-packages.R")
+
+list.files(path="./data")
+s_data<- here::here("data", "installed-packages.csv")   ##How to get the path of the directory, not with the file name
+write_csv(paks, s_data)    ##automatic overwrite??
+
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+
+
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
